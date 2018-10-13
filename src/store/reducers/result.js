b26979c66d2b3f1f62067abadb6d87b7a065035f
alishavirani <alishav@github.com>
2018-10-13 17:49:27 +0530

    completed

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,27 +1,26 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility'
 
 const initialState = {
-    counter: 0,
     results: []
+}
+
+const deleteResult = (state, action) => {
+    const newArray = state.results.filter(result => result.id !== action.resultElemId)
+    return updateObject(state, {results: newArray})
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case actionTypes.STORE_RESULT:
-        return {
-            ...state,
-            results: state.results.concat({value: action.result, id: new Date()})
-        }
-        case actionTypes.DELETE_RESULT:
-        // const id = 2;
-        // const newArray = [...state.results];
-        // newArray.splice(id, 1);
+            return updateObject(state, {results: state.results.concat({value: action.result, id: new Date()})})
 
-        const newArray = state.results.filter(result => result.id !== action.resultElemId)
-        return {
-            ...state,
-            results: newArray
-        }
+        case actionTypes.DELETE_RESULT:
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1);
+
+            return deleteResult(state, action);
     }
     return state;
 }
